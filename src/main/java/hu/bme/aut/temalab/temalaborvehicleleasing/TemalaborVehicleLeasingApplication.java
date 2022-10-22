785689315d9ad7c8840657c73a20cc0ad056dab7
rajmunddongo <rajmund.dongo@gmail.com>
2022-10-22 09:47:38 +0200

    firstcommit

---
+++ src/main/java/hu/bme/aut/temalab/temalaborvehicleleasing/TemalaborVehicleLeasingApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.temalab.temalaborvehicleleasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemalaborVehicleLeasingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemalaborVehicleLeasingApplication.class, args);
+	}
+
+}
