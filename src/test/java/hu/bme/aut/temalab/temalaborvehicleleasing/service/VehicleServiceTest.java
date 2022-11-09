9c79745b486820df807a1d5ced1a3b44b659760d
rajmunddongo <rajmund.dongo@gmail.com>
2022-11-09 21:30:12 +0100

    Vehicle Service and tests

--- src/test/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleServiceTest.java
+++ src/test/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleServiceTest.java
@@ -2,7 +2,10 @@ package hu.bme.aut.temalab.temalaborvehicleleasing.service;
 
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.Bike;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.Car;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Customer;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Rental;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.enums.*;
+import hu.bme.aut.temalab.temalaborvehicleleasing.repository.RentalRepository;
 import hu.bme.aut.temalab.temalaborvehicleleasing.repository.VehicleRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +13,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,8 +30,56 @@ class VehicleServiceTest {
     @Autowired
     private VehicleService vehicleService;
 
+    @Autowired
+    private RentalRepository   rentalRepository;
+
+    private Customer customer;
+    private  Car car;
+    private Rental rent;
+
     @BeforeEach
     void setUp() {
+        //Creating entities
+        car =Car.builder()
+                .vehicleType(VehicleType.CAR)
+                .licensePlate("ASD-123")
+                .seats(5)
+                .mileage(1544)
+                .mileageLimit(300)
+                .horsePower(900)
+                .gearBoxType(GearboxType.MANUAL)
+                .fuelType(FuelType.PETROL_PREMIUM)
+                .carBodyTypes(CarBodyTypes.COUPE)
+                .build();
+        Bike bike = Bike.builder()
+                .vehicleType(VehicleType.BIKE)
+                .licensePlate("ZYZ-143")
+                .seats(2)
+                .mileage(13242)
+                .mileageLimit(300)
+                .horsePower(90)
+                .gearBoxType(GearboxType.MANUAL)
+                .fuelType(FuelType.PETROL)
+                .strokeType(BikeStrokeType.FOUR_STROKE)
+                .build();
+        customer = Customer.builder()
+                .rentals(new ArrayList<Rental>())
+                .build();
+        rent = Rental.builder()
+                .customer(customer)
+                .vehicle(car)
+                .build();
+        Rental rent2 = Rental.builder()
+                .customer(customer)
+                .vehicle(bike)
+                .build();
+        ArrayList<Rental> rentals= new ArrayList<>();
+        rentals.add(rent);
+        rentals.add(rent2);
+        //linking rentals to customer
+        customer.setRentals(rentals);
+
+        rentalRepository.save(rent);
         vehicleRepository.save(
                 Bike.builder()
                         .vehicleType(VehicleType.BIKE)
@@ -38,39 +93,14 @@ class VehicleServiceTest {
                         .strokeType(BikeStrokeType.FOUR_STROKE)
                         .build()
         );
-
-        vehicleRepository.save(
-                Bike.builder()
-                        .vehicleType(VehicleType.BIKE)
-                        .licensePlate("ZYZ-143")
-                        .seats(2)
-                        .mileage(13242)
-                        .mileageLimit(300)
-                        .horsePower(90)
-                        .gearBoxType(GearboxType.MANUAL)
-                        .fuelType(FuelType.PETROL)
-                        .strokeType(BikeStrokeType.FOUR_STROKE)
-                        .build()
-        );
-
-        vehicleRepository.save(
-                Car.builder()
-                        .vehicleType(VehicleType.CAR)
-                        .licensePlate("ASD-123")
-                        .seats(5)
-                        .mileage(1544)
-                        .mileageLimit(300)
-                        .horsePower(900)
-                        .gearBoxType(GearboxType.MANUAL)
-                        .fuelType(FuelType.PETROL_PREMIUM)
-                        .carBodyTypes(CarBodyTypes.COUPE)
-                        .build()
-        );
+        vehicleRepository.save(bike);
+        vehicleRepository.save(car);
     }
 
     @AfterEach
     void tearDown() {
         vehicleRepository.deleteAll();
+        rentalRepository.deleteAll();
     }
 
     @Test
@@ -85,5 +115,18 @@ class VehicleServiceTest {
         assertEquals(250, vehicleService.averageMileageLimitOfVehicleType(VehicleType.BIKE));
         assertEquals(300, vehicleService.averageMileageLimitOfVehicleType(VehicleType.CAR));
         assertEquals(0, vehicleService.averageMileageLimitOfVehicleType(VehicleType.TRANSPORTER));
+    }
+
+    @Test
+    void allMileageTraveled(){ //TDD
+        assertEquals(1544+13242+1234,vehicleService.allMileageTraveled(null));
+    }
+    @Test
+    void mileageTraveledByCustomer(){
+        assertEquals(1544+13242,vehicleService.allMileageTraveled(customer));
+    }
+    @Test
+    void rentalRepositoryFindByVehicle(){
+        assertEquals(rent,rentalRepository.findByVehicle(car));
     }
 }
