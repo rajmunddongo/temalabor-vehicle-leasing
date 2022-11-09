4cb1d49caf9e5443b8b2dae8cb03c02f78f1288b
rajmunddongo <rajmund.dongo@gmail.com>
2022-11-09 21:30:12 +0100

    Vehicle Service and tests

--- src/main/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleService.java
+++ src/main/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleService.java
@@ -1,16 +1,24 @@
 package hu.bme.aut.temalab.temalaborvehicleleasing.service;
 
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Customer;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Rental;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.Vehicle;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.enums.VehicleType;
+import hu.bme.aut.temalab.temalaborvehicleleasing.repository.RentalRepository;
 import hu.bme.aut.temalab.temalaborvehicleleasing.repository.VehicleRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 @RequiredArgsConstructor
-public final class VehicleService {
+public final class VehicleService<vehicles> {
     private final VehicleRepository vehicleRepository;
+    private final RentalRepository rentalRepository;
+
+
 
     /**
      * Calculate average seats for a given type
@@ -36,6 +44,35 @@ public final class VehicleService {
                 .mapToInt(Vehicle::getMileageLimit)
                 .average()
                 .orElse(0);
+    }
+
+    /**
+     * Returns all the mileage traveled, if theres a customer given, gives back the mileage traveled by the customer
+     * @param customer optional param
+     * @return mileage traveled
+     */
+    public int allMileageTraveled(Customer customer){//Customer is NULLABLE!
+        if(customer!=null) return allMileageTraveledByCustomer(customer);
+        int allMileageTraveled = 0;
+        ArrayList<Vehicle> vehicles = (ArrayList<Vehicle>) vehicleRepository.findAll();
+        if(customer==null){
+            for(Vehicle vehicle :vehicles){
+                allMileageTraveled += vehicle.getMileage();
+            }
+        }
+        return allMileageTraveled;
+    }
+    public int allMileageTraveledByCustomer(@NonNull Customer customer){
+        int allMileageTraveled = 0;
+        ArrayList<Vehicle> vehicles = (ArrayList<Vehicle>) vehicleRepository.findAll();
+        ArrayList<Rental> rentals =(ArrayList<Rental>) rentalRepository.findByCustomer(customer);
+        for(Vehicle vehicle :vehicles){
+            for(Rental rental : rentals) {
+                if(rental.getVehicle().equals(vehicle))
+                    allMileageTraveled += vehicle.getMileage();
+            }
+        }
+        return allMileageTraveled;
     }
 
 }
---
+++ src/test/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleCarTest.java
@@ -0,0 +1,58 @@
+package hu.bme.aut.temalab.temalaborvehicleleasing.service;
+
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Bike;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Car;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Vehicle;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.enums.*;
+import hu.bme.aut.temalab.temalaborvehicleleasing.repository.RentalRepository;
+import hu.bme.aut.temalab.temalaborvehicleleasing.repository.VehicleRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class VehicleCarTest {
+    @Mock
+    private VehicleRepository vehicleRepository;
+    @Mock
+    private RentalRepository rentalRepository;
+    private VehicleService vehicleService;
+
+    @BeforeEach
+    void setUp() {
+        vehicleService=new VehicleService(vehicleRepository,rentalRepository);
+    }
+    @AfterEach
+    void tearDown() {
+        vehicleRepository.deleteAll();
+    }
+
+    @Test
+    void testMethodCall() {
+        vehicleService.averageSeatsOfVehicleType(VehicleType.CAR);
+        verify(vehicleRepository,Mockito.times(1)).findByVehicleType(VehicleType.CAR);
+    }
+    @Test
+    void testThrow() {
+        Mockito.when(vehicleRepository.findByVehicleType(VehicleType.valueOf("THROWME")))
+                .thenThrow(IllegalArgumentException.class);
+    }
+
+}
