62dc0144ffdeb68751ece44fa5e9bb097d46b101
Göbhardter Ádám <gobhardter.adam@gmail.com>
2022-11-27 14:34:51 +0100

    Update VehicleCarTest.java
    Removed unnecessary parts. Now it doesn't use database for testing.
    
    Closes #44

--- src/test/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleCarTest.java
+++ src/test/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleCarTest.java
@@ -4,50 +4,40 @@ import hu.bme.aut.temalab.temalaborvehicleleasing.model.enums.*;
 import hu.bme.aut.temalab.temalaborvehicleleasing.repository.RentalRepository;
 import hu.bme.aut.temalab.temalaborvehicleleasing.repository.VehicleRepository;
 import org.apache.logging.log4j.LogManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.mockito.Mockito.verify;
 
-@SpringBootTest
-@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 public class VehicleCarTest {
-    @Mock
+    
+	@Mock
     private VehicleRepository vehicleRepository;
+	
     @Mock
     private RentalRepository rentalRepository;
+    
+    @InjectMocks
     private VehicleService vehicleService;
-
-    @BeforeEach
-    void setUp() {
-        vehicleService=new VehicleService(vehicleRepository,rentalRepository);
-    }
-    @AfterEach
-    void tearDown() {
-        vehicleRepository.deleteAll();
-    }
 
     @Test
     void testMethodCall() {
         vehicleService.averageSeatsOfVehicleType(VehicleType.CAR);
         verify(vehicleRepository,Mockito.times(1)).findByVehicleType(VehicleType.CAR);
     }
+    
     @Test
     void testThrow() {
         try {
-                Mockito.when(vehicleRepository.findByVehicleType(VehicleType.valueOf("THROWME")))
-                        .thenThrow(IllegalArgumentException.class);
+            Mockito.when(vehicleRepository.findByVehicleType(VehicleType.valueOf("THROWME")))
+                    .thenThrow(IllegalArgumentException.class);
         }catch(IllegalArgumentException e ){
             LogManager.getLogger().info("IllegalArgumentException catched");
         }
     }
-
 }
