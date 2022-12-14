36fba304e7c37c2899bdc6bea1eab3c500092698
rajmunddongo <rajmund.dongo@gmail.com>
2022-12-14 12:44:07 +0100

    Added more services and tested them. new service for filterVechicleByFuelType,filterVehicleByFuelAndVehicleType,vehicleSortedByMileageLimit and a method to find supercars

--- src/main/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleService.java
+++ src/main/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/VehicleService.java
@@ -3,6 +3,7 @@ package hu.bme.aut.temalab.temalaborvehicleleasing.service;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.Customer;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.Rental;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.Vehicle;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.enums.FuelType;
 import hu.bme.aut.temalab.temalaborvehicleleasing.model.enums.VehicleType;
 import hu.bme.aut.temalab.temalaborvehicleleasing.repository.RentalRepository;
 import hu.bme.aut.temalab.temalaborvehicleleasing.repository.VehicleRepository;
@@ -10,7 +11,8 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.function.Predicate;
 
 @Service
 @RequiredArgsConstructor
@@ -76,4 +78,64 @@ public final class VehicleService{
         }
         return allMileageTraveled;
     }
+
+    /**
+     * Filter vehicles by their type of fuel
+     * @param fuelType
+     * @return filtered list
+     */
+    public List<Vehicle> filterVehicleByFuelType(FuelType fuelType){
+        return vehicleRepository.findByFuelType(fuelType).stream().distinct().toList();
+    }
+
+    /**
+     * Filter vehicles by their type and their fuel type in the same time
+     * @param fuelType
+     * @param vehicleType
+     * @return filtered list
+     */
+    public List<Vehicle> filterVehicleByFuelAndVehicleType(FuelType fuelType,VehicleType vehicleType){
+        ArrayList<Vehicle> filteredvehicles = new ArrayList<>();
+        for(Vehicle v1 : new ArrayList<>(vehicleRepository.findByFuelType(fuelType).stream().distinct().toList())){
+            for(Vehicle v2 :  new ArrayList<>(vehicleRepository.findByVehicleType(vehicleType).stream().distinct().toList())){
+                if(v1.getId().equals(v2.getId())) filteredvehicles.add(v1);
+            }
+        }
+        return filteredvehicles;
+    }
+
+    /**
+     * Sort vehicles by their mile limit.
+     * @return sortedList
+     */
+    public List<Vehicle>  vehiclesSortedByMileLimit(){
+        return vehicleRepository.findAll().stream().distinct().sorted(new Comparator<Vehicle>() {
+            @Override
+            public int compare(Vehicle o1, Vehicle o2) {
+                return -o1.getMileageLimit()+o2.getMileageLimit();
+            }
+        }).toList();
+    }
+
+    /**
+     * Returns supercars. A supercar means a car with horsepower in the top 30% and 2 or less seats.
+     * @return filtered list
+     */
+    public List<Vehicle> superCars(){
+        double  size= vehicleRepository.findAll().stream().toList().size();
+        double limitofhorsepower = vehicleRepository.findAll().stream().sorted(new Comparator<Vehicle>() {
+            @Override
+            public int compare(Vehicle o1, Vehicle o2) {
+                return -o1.getHorsePower()+o2.getHorsePower();
+            }
+        }).toList().get((int)Math.floor((size*0.3))).getHorsePower();
+        return vehicleRepository.findAll().stream().filter(new Predicate<Vehicle>() {
+            @Override
+            public boolean test(Vehicle vehicle) {
+                if(vehicle.getSeats()<=2 && vehicle.getHorsePower()>=limitofhorsepower) return true;
+                return false;
+            }
+        }).toList();
+    }
+
 }
