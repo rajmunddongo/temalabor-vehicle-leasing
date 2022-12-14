39d9d29f80e565a019941d9bcf2d76b18bfeb1c9
Adrian Szell <adrian@szell.dev>
2022-12-14 16:41:15 +0100

    Introduce tests for UserDetailsServiceImpl.java in UserDetailsServiceImplTest.java, 100% coverage
    closes #52

---
+++ src/test/java/hu/bme/aut/temalab/temalaborvehicleleasing/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,60 @@
+package hu.bme.aut.temalab.temalaborvehicleleasing.service;
+
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.Customer;
+import hu.bme.aut.temalab.temalaborvehicleleasing.model.enums.UserRole;
+import hu.bme.aut.temalab.temalaborvehicleleasing.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserDetailsServiceImplTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserDetailsServiceImpl udsi;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(
+                Customer.builder()
+                        .firstName("Got")
+                        .lastName("You")
+                        .username("gotcha")
+                        .password("getthis")
+                        .drivingLicenceNumber("AAAAAAAA2")
+                        .bonusPoints(5)
+                        .build()
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void UserDetailsServiceImplTest() {
+        UserDetails user = udsi.loadUserByUsername("gotcha");
+        assertTrue(user.getAuthorities().contains(UserRole.ROLE_CUSTOMER));
+        assertTrue(user.getPassword().contains("getthis"));
+
+    }
+
+    @Test
+    void TestUsernameIsEmpty() {
+        Executable executable = () -> udsi.loadUserByUsername("InvalidUserName");
+        Exception exception = assertThrows(UsernameNotFoundException.class, executable);
+        assertTrue(exception.getMessage().contains("User not found!"));
+    }
+}
